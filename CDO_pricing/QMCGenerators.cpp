55ec957974435afceabd430191c00b91e7c93e15
plccrl <carlo.pulcini@hotmail.it>
2017-07-04 20:18:30 +0200

    Kakutani

--- CDO_pricing/QMCGenerators.cpp
+++ CDO_pricing/QMCGenerators.cpp
@@ -76,3 +76,95 @@ std::vector<double> MCPROJ::Halton2DNIG::operator()()
 	r.push_back((lol[1] * sqrt(y[1])) + m_mu_2 + (m_beta_2*y[1]));
 	return r;
 }
+
+std::vector<double> MCPROJ::Kakutani2D::double2piadic(double x, size_t base)
+{
+	std::vector<int> res(m_decimals);
+	div_t divresult;
+	for (int i = 0; i < m_deciamls; i++)
+	{
+		res[i] = (int)floor(x * base);
+		x -= res[i];
+	}
+	return res;
+}
+
+double MCPROJ::Kakutani2D::piadic2double(std::vector<int> p, site_t base)
+{
+	double res = 0.0;
+
+	for (int i = 0; i < m_deciamls; i++)
+	{
+		res += p[i] * pow(base, -(i + 1));
+	}
+
+	return res;
+}
+
+
+std::vector<double> MCPROJ::Kakutani2D::operator()()
+{
+	std::vector<double> r(2);
+
+	m_previous[0][0] += 1;
+	m_previous[1][0] += 1;
+
+	for (int i = 0; i < m_decimals - 1; i++)
+	{
+		if (m_previous[0][i] == m_base_1)
+		{
+			m_previous[0][i] = 0;
+			m_previous[0][i + 1] + 1;
+		}
+		if (m_previous[1][i] == m_base_2)
+		{
+			m_previous[1][i] = 0;
+			m_previous[1][i + 1] + 1;
+		}
+	}
+	if (m_previous[0][m_decimals - 1] == m_base_1)
+		m_previous[0][m_decimals - 1] = 0;
+	if (m_previous[1][m_decimals - 1] == m_base_2)
+		m_previous[1][m_decimals - 1] = 0;
+
+	r[0] = padic2double(m_previous[0], m_base_1);
+	r[1] = padic2double(m_previous[1], m_base_2);
+	return r;
+}
+
+std::vector<double> MCPROJ::Kakutani2DGauss::operator()()
+{
+	static const double epsilon = std::numeric_limits<double>::min();
+	static const double two_pi = 2.0*3.14159265358979323846;
+	std::vector<double> z;
+	double u1, u2;
+	do
+	{
+		std::vector<double> temp = m_K2D ->operator()();
+		u1 = temp[0];
+		u2 = temp[1];
+	} while (u1 <= epsilon);
+
+	z.push_back((sqrt(-2.0 * log(u1)) * cos(two_pi * u2))*m_sigma + m_mu);
+	z.push_back((sqrt(-2.0 * log(u1)) * sin(two_pi * u2))*m_sigma + m_mu);
+	return z;
+}
+
+std::vector<double> MCPROJ::Kakutani2DNIG::operator()()
+{
+	// getting uniform random variable value between 0 and 1
+	std::vector<double> x = m_K2D->operator()();
+	// getting value of IG distribution
+
+	std::vector<double> y;
+	y.push_back(inverseIG(x[0], m_Listofvalues_1));
+	y.push_back(inverseIG(x[1], m_Listofvalues_2));
+
+	std::vector<double> lol = m_KG2D -> operator()();
+
+	//returning value of NIG distribution
+	std::vector<double> r;
+	r.push_back((lol[0] * sqrt(y[0])) + m_mu_1 + (m_beta_1*y[0]));
+	r.push_back((lol[1] * sqrt(y[1])) + m_mu_2 + (m_beta_2*y[1]));
+	return r;
+}
--- CDO_pricing/QMCGenerators.h
+++ CDO_pricing/QMCGenerators.h
@@ -67,10 +67,13 @@ namespace MCPROJ {
 
 	public:
 		//constructor
-		Halton2DNIG(double			alpha_1, double alpha_2,
-					double			beta_1, double beta_2,
-					double			mu_1, double mu_2,
-					double			delta_1, double delta_2, int grid_size, size_t base_1 = 2, size_t base_2 = 3, size_t base_3 = 5, size_t base_4 = 7)
+		Halton2DNIG(double alpha_1, double alpha_2,
+					double beta_1, double beta_2,
+					double mu_1, double mu_2,
+					double delta_1, double delta_2, 
+					int grid_size, 
+					size_t base_1 = 2, size_t base_2 = 3, 
+					size_t base_3 = 5, size_t base_4 = 7)
 			:m_index(1), m_alpha_1(alpha_1), m_alpha_2(alpha_2), m_beta_1(beta_1), m_beta_2(beta_2),
 			m_mu_1(mu_1), m_mu_2(mu_2), 	m_delta_1(delta_1), m_delta_2(delta_2), m_gridSize(grid_size)
 		{	
@@ -79,6 +82,110 @@ namespace MCPROJ {
 			else if (delta_1 < 1e-30|| delta_2 < 1e-30) { throw std::exception(" value of delta is negative "); }
 			m_H2D = new Halton2D(base_1, base_2);
 			m_HG2D = new Halton2DGauss(0.0, 1.0, base_3, base_4);
+
+			double gamma_1 = sqrt(alpha_1*alpha_1 - beta_1*beta_1);
+			double gamma_2 = sqrt(alpha_2*alpha_2 - beta_2*beta_2);
+
+			m_IG_1 = inverse_gaussian_dist(m_delta_1*gamma_1, gamma_1*gamma_1);
+			m_IG_2 = inverse_gaussian_dist(m_delta_2*gamma_2, gamma_2*gamma_2);
+
+			for (int i = 0; i<m_gridSize; i++)
+			{
+				m_Listofvalues_1.push_back(boost::math::quantile(m_IG_1, ((double)i) / m_gridSize));
+				m_Listofvalues_2.push_back(boost::math::quantile(m_IG_2, ((double)i) / m_gridSize));
+			}
+		};
+
+		std::vector<double> operator()();
+
+	};
+
+	class Kakutani2D {
+
+		size_t								m_index;
+		size_t								m_base_1;
+		size_t								m_base_2;
+		std::vector<std::vector<int>>		m_previous;
+		int									m_decimals
+
+	public:
+		//constructor
+		Kakutani2D(size_t base_1, size_t base_2, int N) :m_base_1(base_1), m_base_2(base_2), m_index(1) , m_decimals(N)
+		{
+			m_previous[0] = std::vector<int>(N);
+			m_previous[1] = std::vector<int>(N);
+
+			double x0, x1;
+			if ((base_1 = 5) || (base_1 == 7))
+				x0 = (2 * base_1 - 1 - sqrt((base_1 + 2)*(base_1 + 2) + 4 * base_1)) / 3.0;
+			else
+				x0 = 1 / (double)base_1;
+			if ((base_2 = 5) || (base_2 == 7))
+				x1 = (2 * base_2 - 1 - sqrt((base_2 + 2)*(base_2 + 2) + 4 * base_2)) / 3.0;
+			else
+				x1 = 1 / (double)base_2;
+
+			m_previous[0] = double2piadic(x0, m_base_1);
+			m_previous[1] = double2piadic(x1, m_base_2);
+		};
+
+		std::vector<int> double2piadic(double x, size_t base);
+
+		double piadic2double(std::vector<int> p, site_t base);
+
+		std::vector<double> operator()();
+
+	};
+
+	class Kakutani2DGauss {
+
+		size_t			m_index;
+		Kakutani2D	*	m_K2D;
+		double			m_mu;
+		double			m_sigma;
+
+	public:
+		//constructor
+		Kakutani2DGauss(double mu = 0.0, double sigma = 1.0, size_t base_1 = 2, size_t base_2 = 3, int N=10) :m_index(1), m_mu(mu), m_sigma(sigma) {
+			m_K2D = new Kakutani2D(base_1, base_2, N);
+		};
+
+		std::vector<double> operator()();
+
+	};
+
+	class Kakutani2DNIG {
+
+		size_t				m_index;
+		Kakutani2D		*	m_K2D;
+		Kakutani2DGauss *	m_KG2D;
+		double				m_alpha_1, m_alpha_2;
+		double				m_beta_1, m_beta_2;
+		double				m_mu_1, m_mu_2;
+		double				m_delta_1, m_delta_2;
+		int					m_gridSize;
+		std::vector<double> m_Listofvalues_1;
+		std::vector<double> m_Listofvalues_2;
+		inverse_gaussian_dist		m_IG_1;
+		inverse_gaussian_dist		m_IG_2;
+
+	public:
+		//constructor
+		Halton2DNIG(double alpha_1, double alpha_2,
+					double beta_1, double beta_2,
+					double mu_1, double mu_2,
+					double delta_1, double delta_2, 
+					int grid_size, 
+					size_t base_1 = 2, size_t base_2 = 3, 
+					size_t base_3 = 5, size_t base_4 = 7)
+			:m_index(1), m_alpha_1(alpha_1), m_alpha_2(alpha_2), m_beta_1(beta_1), m_beta_2(beta_2),
+			m_mu_1(mu_1), m_mu_2(mu_2), m_delta_1(delta_1), m_delta_2(delta_2), m_gridSize(grid_size)
+		{
+			if (abs(beta_1) > alpha_1 || abs(beta_2) > alpha_2) { throw std::exception(" value of abs(beta) is greater than alpha "); }
+
+			else if (delta_1 < 1e-30 || delta_2 < 1e-30) { throw std::exception(" value of delta is negative "); }
+			m_K2D = new Kakutani2D(base_1, base_2);
+			m_KG2D = new Kakutani2DGauss(0.0, 1.0, base_3, base_4);
 
 			double gamma_1 = sqrt(alpha_1*alpha_1 - beta_1*beta_1);
 			double gamma_2 = sqrt(alpha_2*alpha_2 - beta_2*beta_2);
